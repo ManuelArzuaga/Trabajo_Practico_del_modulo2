b54c82dfba3bcaeb4c0814c0f031d911abf6d7f7
arzuagamanuel <71405710+ManuelArzuaga@users.noreply.github.com>
2025-07-02 16:18:32 -0300

    Sexto commit

--- src/Views/Registro.jsx
+++ src/Views/Registro.jsx
@@ -6,16 +6,32 @@ function Registro(){
 
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
+  const [error,setError] = useState(null)
+
+
+   function handleSumbit(e){
+
+    e.preventDefault()
+
+    if((!email) || (!password)){
+    setError("Debes completar los campos")
+    return
+    }
+    else{
+      setError(null)
+    }
+  }
 
   return(
     <Layout>
          <h1>Registro</h1>
-      <form>
+      <form onSubmit={handleSumbit}>
         <label htmlFor="email"><b>Correo Electronico:</b></label>
         <input name="email" id="email" type="email" onChange={(event)=>setEmail(event.target.value)}></input>
         <label htmlFor="password"><b>Contraseña:</b></label>
         <input name="password" id="password" type="password" onChange={(event)=>setPassword(event.target.value)}></input>
         <button type="submmit">Ingresar</button>
+        {error && <p>{error}</p>}
       </form>
     </Layout>
   )
